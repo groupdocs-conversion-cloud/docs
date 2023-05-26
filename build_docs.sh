d6bb40e5cfc7d98f4d09299cf6f93822b927c24c
SergeiTerentev <Sergei.Terentev@Aspose.com>
2023-05-26 15:32:17 +0300

    Update common

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-git submodule update --init --recursive docs-cloud-common
+git submodule init
+git submodule update
+git submodule foreach 'git fetch origin; git checkout $(git rev-parse --abbrev-ref HEAD); git reset --hard origin/$(git rev-parse --abbrev-ref HEAD); git submodule update --recursive; git clean -dfx'
 rm -rf docs-cloud-common/content/conversion
 cp -R content docs-cloud-common
 cd docs-cloud-common
